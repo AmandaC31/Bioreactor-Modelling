13192fd0a0bf275964f76cf69181a01cdfb0b67a
AmandaC31 <98112842+AmandaC31@users.noreply.github.com>
2022-11-26 17:20:24 -0500

    Update AppBioreactor.py

--- AppBioreactor.py
+++ AppBioreactor.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
